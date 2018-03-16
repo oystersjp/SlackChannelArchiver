1582c43b98759dbfbe337dbaeac36a05b4580eb5
saitota <1152469+saitota@users.noreply.github.com>
2018-03-16 14:55:28 +0900

    changed to LEGACY-TOKEN for archive

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import logging
 import urllib.request
 import os
-import datetime
 from datetime import datetime
 import time
 
@@ -10,94 +9,105 @@ print('Loading function... ')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
-#def event_to_json(event):
-#    if 'body' in event:
-#        body = json.loads(event.get('body'))
-#        return body
-#    elif 'token' in event:
-#        body = event
-#        return body
-#    else:
-#        logger.error('unexpected event format')
-#        exit
-
-
-#class ChallangeJson(object):
-#    def data(self, key):
-#        return {
-#            'isBase64Encoded': 'true',
-#            'statusCode': 200,
-#            'headers': {},
-#            'body': key
-#        }
-
-
 class PostJson(object):
     def __init__(self):
-        self.BOT_TOKEN = os.environ['BOT_TOKEN']
+        self.BOT_TOKEN =  os.environ['BOT_TOKEN']
         self.OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+        self.BOT_MESSAGE = os.environ['BOT_MESSAGE']
+        self.BOT_ICON = os.environ['BOT_ICON']
+        self.BOT_NAME =os.environ['BOT_NAME']
+        self.LEGACY_TOKEN = os.environ['LEGACY_TOKEN']
     def headers(self):
         return {
             'Content-Type': 'application/json; charset=UTF-8',
             'Authorization': 'Bearer {0}'.format( self.BOT_TOKEN)
         }
-    def data(self):
+    def data_list(self):
         return {
             'token': self.OAUTH_TOKEN,
             'exclude_archived': 'true'
         }
+    def data_hist(self,channel):
+        return {
+                'token': self.OAUTH_TOKEN,
+                'channel': channel,
+                'count': 1
+            }
+    def data_message(self, channel):
+        return {
+            'token': self.OAUTH_TOKEN,
+            'channel': channel,
+            'text': self.BOT_MESSAGE,
+            'username': self.BOT_NAME,
+            'icon_emoji': self.BOT_ICON
+        }
+    def headers_archive(self):
+        return {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer {0}'.format( self.LEGACY_TOKEN)
+        }
+    def data_archive(self, channel):
+        return {
+            'token': self.LEGACY_TOKEN,
+            'channel': channel
+        }
+
 
 
 def handler(event, context):
     # Output the received event to the log
-    logging.info(json.dumps(event))
-    #body = event_to_json(event)
-    BOT_MESSAGE = os.environ['BOT_MESSAGE']
-    OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+    #logging.info(json.dumps(event))
     ARCHIVE_AFTER_DAYS = os.environ['ARCHIVE_AFTER_DAYS']
-    BOT_ICON = os.environ['BOT_ICON']
-    BOT_NAME = os.environ['BOT_NAME']
-    BOT_TOKEN = os.environ['BOT_TOKEN']
-    # return if it was challange-event
-    #if 'challenge' in body:
-    #    challenge_key = body.get('challenge')
-    #    logging.info('return challenge key %s:', challenge_key)
-    #    return ChallangeJson().data(challenge_key)
-
     # list channel
     url = 'https://slack.com/api/channels.list'
     post_head = PostJson().headers()
-    post_body = PostJson().data()
+    post_body = PostJson().data_list()
     req = urllib.request.Request(url,data=json.dumps(post_body).encode('utf-8'), method='POST', headers=post_head)
     res = urllib.request.urlopen(req)
     logger.info('post result: %s', res.msg)
     channels = json.loads(res.read().decode('utf8'))
 
-    #print(channels)
-    # chekc old channel
+    # get channels info / require scope :channels:history
     for channel in channels.get('channels'):
-        url = "https://slack.com/api/channels.info"
-        params = urllib.parse.urlencode({
-                'token': BOT_TOKEN,
-                'channel': channel.get('id')
-            })
-        params = params.encode('utf-8')
-        req = urllib.request.Request(url, params)
-        response = urllib.request.urlopen(req).read()
-        channelsinfo = json.loads(response.decode('utf-8'))
-        print(channelsinfo)
-        channel = channelsinfo.get('channel',{})
-        latest = channel.get('latest',{})
-        ts = latest.get('ts','1000000000') # epoch 2001/9/9 10:46:40
-        dt = datetime.fromtimestamp(float(ts))
-        now = datetime.now()
-        diff = now - dt 
-        if ( int(diff.days) > int(ARCHIVE_AFTER_DAYS) ):
-            logger.info('target channel: %s',  channel.get('name',''))
-            # todo: message to target channel
-            
-            # todo: archive target channel
+        url = "https://slack.com/api/channels.history" # does not support application/json
+        post_body = PostJson().data_hist(channel.get('id'))
+        req = urllib.request.Request(url, urllib.parse.urlencode(post_body).encode('utf-8'))
+        res = urllib.request.urlopen(req)
+        channelhist = json.loads(res.read().decode('utf-8'))
+        #print(channelhist)
+        messages = channelhist.get('messages')
+        ts = messages[0].get('ts','1000000000') # epoch 2001/9/9 10:46:40
+        ts_datetime = datetime.fromtimestamp(float(ts))
+        now_datetime = datetime.now()
+        diff_datetime = now_datetime - ts_datetime 
+        # check old channels
+        if ( int(diff_datetime.days) > int( ARCHIVE_AFTER_DAYS) ):
+            logger.info('target channel to archive: %s',  channel.get('name',''))
+            # message to target channel / require scope : chat:write:user
+            post_data = PostJson().data_message(channel.get('id'))
+            url = 'https://slack.com/api/chat.postMessage'
+            req = urllib.request.Request(
+                url,
+                data=json.dumps(post_data).encode('utf-8'),
+                method='POST',
+                headers=post_head)
+            res = urllib.request.urlopen(req)
+            logger.info('message result: %s', res.msg)
+            # archive target channel / require scope : LEGACY TOKEN
+            post_head = PostJson().headers_archive()
+            post_data = PostJson().data_archive(channel.get('id'))
+            url = 'https://slack.com/api/channels.archive'
+            req = urllib.request.Request(
+                url,
+                data=json.dumps(post_data).encode('utf-8'),
+                method='POST',
+                headers=post_head)
+            res = urllib.request.urlopen(req)
+            logger.info('archive result: %s', res.msg)
+            #load = json.loads(res.read().decode('utf8'))
+            #print (load)
+            return 'ok' # archive 1 channel and exit fnc, comment-out if you want archive every old channel
+
 
     return 'ok'
 
