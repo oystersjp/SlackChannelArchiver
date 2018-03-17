1fbfaed04bcb87ca9393a7c9ff4ad6b52c378c81
saitota <1152469+saitota@users.noreply.github.com>
2018-03-17 12:51:09 +0900

    refactor

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ logger.setLevel(logging.INFO)
 
 class PostJson(object):
     def __init__(self):
-        #self.BOT_TOKEN =  os.environ['BOT_TOKEN']
-        #self.OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
-        self.BOT_MESSAGE = os.environ['BOT_MESSAGE']
-        self.BOT_ICON = os.environ['BOT_ICON']
-        self.BOT_NAME = os.environ['BOT_NAME']
         self.LEGACY_TOKEN = os.environ['LEGACY_TOKEN']
+        #self.OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+        #self.BOT_TOKEN =  os.environ['BOT_TOKEN']
+        self.BOT_NAME = os.environ['BOT_NAME']
+        self.BOT_ICON = os.environ['BOT_ICON']
+        self.BOT_MESSAGE = os.environ['BOT_MESSAGE']
 
     def headers(self):
         return {
@@ -69,7 +69,7 @@ def handler(event, context):
 
     # get channels info / require scope :channels:history
     for channel in channels.get('channels'):
-        logger.info('checl channel: %s ', channel.get('name', ''))
+        logger.info('check channel: %s ', channel.get('name', ''))
         url = "https://slack.com/api/channels.history"  # does not support application/json
         post_body = PostJson().data_hist(channel.get('id'))
         req = urllib.request.Request(
@@ -77,10 +77,10 @@ def handler(event, context):
             urllib.parse.urlencode(post_body).encode('utf-8'))
         res = urllib.request.urlopen(req)
         channelhist = json.loads(res.read().decode('utf-8'))
-        #print(channelhist)
+        # get timestamp of latest message
         messages = channelhist.get('messages')
         message = messages.pop(0) if messages else {}
-        ts = message.get('ts', '1000000000')  # epoch 2001/9/9 10:46:40
+        ts = message.get('ts', '1000000000')  # epoch 2001/9/9 10:46:40 if blank
         ts_datetime = datetime.fromtimestamp(float(ts))
         now_datetime = datetime.now()
         diff_datetime = now_datetime - ts_datetime
@@ -109,8 +109,7 @@ def handler(event, context):
                 headers=post_head)
             res = urllib.request.urlopen(req)
             logger.info('archive result: %s', res.msg)
-            #load = json.loads(res.read().decode('utf8'))
-            #print (load)
+
             return 'ok'  # archive 1 channel and exit function, comment-out if you want archive every old channel
 
     return 'ok'
